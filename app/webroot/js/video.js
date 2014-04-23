33470ac0fa18e742bd3eb6a424d33f7600d92c45
jam2z9 <jam2z9@mail.missouri.edu>
2014-04-23 14:18:36 -0500

    Update video.js

--- app/webroot/js/video.js
+++ app/webroot/js/video.js
@@ -2,7 +2,7 @@
     $("#youtube").osmplayer({
       width: '100%',
       height: '600px',
-      playlist: 'http://www.youtube.com/watch?v=qebEy8p6pAY&list=PLzzy8Igfmn1SHfMFOHVM6dDGQz7p1wDfg'
+      playlist: 'https://gdata.youtube.com/feeds/api/users/JclLiFiaEIrSXSlI8VKuIw/uploads'
     });
   });
 
