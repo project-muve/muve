c79645e76d40f3441638a144d08a431208eb9465
jam2z9 <jam2z9@mail.missouri.edu>
2014-04-23 14:36:48 -0500

    Update video.js

--- app/webroot/js/video.js
+++ app/webroot/js/video.js
@@ -2,7 +2,8 @@
     $("#youtube").osmplayer({
       width: '100%',
       height: '600px',
-      playlist: 'https://gdata.youtube.com/feeds/api/users/Mizzouwellness/uploads'
+      //playlist: 'https://gdata.youtube.com/feeds/api/users/Mizzouwellness/uploads'
+      playlist: 'https://gdata.youtube.com/feeds/api/users/bVnkz2Giqz9LbhBfaL0AtA/uploads'
      // playlist: 'https://gdata.youtube.com/feeds/api/users/JclLiFiaEIrSXSlI8VKuIw/uploads'
     });
   });
