cbd84d53a36fb5eba1d9c8102937717fcd8cf5eb
Barrek Roberts <bar535@mail.missouri.edu>
2014-04-26 15:57:20 -0500

    Fixes: Removed Email Debugger from user view added saving logic to muvetools

--- app/Model/MuveTool.php
+++ app/Model/MuveTool.php
@@ -6,5 +6,24 @@ App::uses('AppModel', 'Model');
 */
 class MuveTool extends AppModel {
 public $useTable = 'muve_tools';
+
+/**
+ * Ensure the URL is explicit before saving
+ * 
+ * @param Array $options  
+ * 
+ * @return Bool
+ */
+
+public function beforeSave($options = array()) {
+    if (!empty($this->data['MuveTool']['url'])) {
+		if (stristr($this->data['MuveTool']['url'],'http'))
+		{
+			$this->data['MuveTool']['url']='http://' . $this->data['MuveTool']['url'];
+		}
+    }
+    return true;
+}
+
 }
 
