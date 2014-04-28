9753002f790038a717de554ed0c45e3196d8dfe0
jam2z9 <jam2z9@mail.missouri.edu>
2014-04-27 20:34:03 -0500

    Update ArticlesController.php

--- app/Controller/ArticlesController.php
+++ app/Controller/ArticlesController.php
@@ -110,11 +110,11 @@ class ArticlesController extends AppController {
 		$this->set(compact('users'));
 	}
 /*     Function to display most recent article posts!      */
-public function lastpost($limit = 4) {
+public function lastpost($limit = 3) {
 
-	$articles = $this->Article->find('all', array('fields'=>array('Article.id', 'Article.title', 'Article.ts_posted'),
+	$article = $this->Article->find('all', array('fields'=>array('Article.id', 'Article.title', 'Article.ts_posted'),
 							   'recursive'=>0,
-							   'order'=>array('Article.ts_posted description'),
+							   'order'=>array('Article.ts_posted'),
 							   'limit'=>$limit));
  
 	if(isset($this->params['requested']))
